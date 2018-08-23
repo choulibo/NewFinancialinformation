1054a45a308439355203d9bfa935c8e1c65e91e2
choulibo <choulibo@gmail.com>
2018-08-23 18:52:27 +0800

    进行数据库的配置

--- manage.py
+++ manage.py
@@ -3,6 +3,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+class Config(object):
+    """工程配置信息"""
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "mysql:root:root@127.0.0.1:3306/information17"
+    SQLALCHEMY_TRACK_MODIFYTION = False
+# 从配置对象中加载
+app.config.from_object(Config)
+
 
 @app.route('/')
 def index():
