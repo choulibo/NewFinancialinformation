2dfd4911519f7dc1b73a21ad9ab6daa5f820f21a
choulibo <choulibo@gmail.com>
2018-08-24 19:13:37 +0800

    集成日志到项目，根据不同环境配置不同的日志

--- info/__init__.py
+++ info/__init__.py
@@ -12,9 +12,9 @@ from config import config
 db = SQLAlchemy()
 
 
-def setup_log():
+def setup_log(config_name):
     # 设置日志的记录等级
-    logging.basicConfig(level=logging.DEBUG)  # 调试debug级日志文件个数上限
+    logging.basicConfig(level=config[config_name].LOG_LEVEL)  # 调试debug级日志文件个数上限
     # 创建日志记录器，指明日志保存的路径每个日志文件的大小，保存的
     file_log_handler = RotatingFileHandler('logs/log', maxBytes=1024 * 1024 * 100, backupCount=10)
     # 创建日志记录的格式，日志等级，输入日志信息的文件名　行数　日志信息
@@ -25,7 +25,9 @@ def setup_log():
     logging.getLogger().addHandler(file_log_handler)
 
 def create_app(config_name):
-    setup_log()
+    # 配置日志并且传入配置名字，以便能获取到指定配置所对应的日志等级
+    setup_log(config_name)
+    # 创建flask 对象
     app = Flask(__name__)
     # 从配置对象中加载
     app.config.from_object(config[config_name])
