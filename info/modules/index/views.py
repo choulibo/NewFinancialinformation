d8e948fd301a377fb68211c41e60b3bb05e98dbc
choulibo <choulibo@gmail.com>
2018-08-31 23:40:54 +0800

    主页的登录的状态显示实现

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,14 +1,35 @@
 # coding = utf-8
-from flask import render_template, current_app
+from flask import render_template, current_app, session
 
 from info import redis_store
+from info.models import User
 from . import index_blu
 
 
 # 创建路由
 @index_blu.route('/')
 def index():
-    return render_template('news/index.html')
+    """
+    显示主页
+    1.如果用户已经登录,将当前用户的数据传到模板中,供模板显示
+    :return:
+    """
+    # 取到用户id
+    user_id = session.get("user_id",None)
+    user = None
+    if user_id:
+        # 尝试查询用户的模型
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+
+    data = {
+        "user": user.to_dict()
+
+    }
+
+    return render_template('news/index.html',data = data)
 
 # send_static_file 是flask 去查找指定文件的静态文件所调用的方法
 @index_blu.route('/favicon.ico')
